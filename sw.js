8eb6edc86a1f4aa5d6d6171ce2e568986f5050f4
enochsofficial2 <enochsofficial2@gmail.com>
2026-07-13 17:33:54 +0900

    자동 업데이트: sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'sellerhub-v4';
-const CORE_ASSETS = ['./index.html', './orders.html', './inventory.html', './profile.html', './insights.html', './style.css', './common.js', './manifest.json'];
+const CACHE_NAME = 'sellerhub-v5';
+const CORE_ASSETS = ['./index.html', './orders.html', './inventory.html', './profile.html', './insights.html', './print.html', './style.css', './common.js', './manifest.json'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
